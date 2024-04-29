3c7a08a8301ec570c3c869b4cf935edce05f05ef
Md Sohel Rana <mdsohelrana77773@gmail.com>
2024-04-29 11:04:58 +0600

    Push the build file

---
+++ public/build/assets/Settings-bdc4eed8.js
@@ -0,0 +1,1 @@
+import{j as t}from"./app-5d40b231.js";function i(){return t.jsx("div",{children:"Settings"})}export{i as default};
--- public/build/assets/Settings-e086b7e5.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./app-f645d299.js";function i(){return t.jsx("div",{children:"Settings"})}export{i as default};
